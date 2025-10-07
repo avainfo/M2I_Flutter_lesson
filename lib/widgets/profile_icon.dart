6b4aef3b00bbe59f80e9b7e8ff03aaf0c37fa7ac
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-07 14:42:23 +0200

    feat: Update profile icon to use network image

--- lib/widgets/profile_icon.dart
+++ lib/widgets/profile_icon.dart
@@ -9,11 +9,11 @@ class ProfileIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(100),
-      child: Image(
+      child: Image.network(
+        "https://static.vecteezy.com/system/resources/thumbnails/053/741/746/small/a-colorful-lizard-with-a-blue-and-orange-face-is-staring-at-the-camera-the-lizard-s-face-is-the-main-focus-of-the-image-and-it-is-curious-or-alert-the-bright-colors-of-the-lizard-s-face-photo.jpg",
         width: 75,
         height: 75,
         fit: BoxFit.cover,
-        image: AssetImage("pfp.jpg"),
       ),
     );
   }
