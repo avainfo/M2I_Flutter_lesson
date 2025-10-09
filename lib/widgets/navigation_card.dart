b5a742c4fb5407402565cd969fd052f30bf4833c
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-09 09:22:40 +0200

    feat: Implement onTap behavior for NavigationCard
    Adds a required `event` function to the `NavigationCard` widget. This function is triggered by the `onTap` event, allowing the card to execute a given action when tapped.

--- lib/widgets/navigation_card.dart
+++ lib/widgets/navigation_card.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 
 class NavigationCard extends StatelessWidget {
   final IconData icon;
+  final Function event;
 
   const NavigationCard({
     super.key,
     this.icon = Icons.question_mark_rounded,
+    required this.event,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(25),
-      onTap: () {},
+      onTap: () => event(),
       splashColor: Colors.greenAccent,
       child: Container(
         height: MediaQuery.of(context).size.width / 2,
