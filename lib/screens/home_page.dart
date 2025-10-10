1b21308254a1568d164bf69688fd1a2fd854e383
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-10 14:12:06 +0200

    Refactor: Reorder imports in home_page.dart
    The import statements in `lib/screens/home_page.dart` have been reordered.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:lesson_m2i_lyon_flutter/models/todo.dart';
 import 'package:lesson_m2i_lyon_flutter/screens/base_page.dart';
-import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
